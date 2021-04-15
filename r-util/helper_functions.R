22410053ddeb909a97d633de6dfeb2b4875d4f9b
Josip Domazet <josip.domazet@live.com>
2021-04-15 15:11:19 +0200

    Helper functions

---
+++ r-util/helper_functions.R
@@ -0,0 +1,25 @@
+plot_one_factor <- function(tbl, var) {
+  tbl %>%
+    count(var = fct_explicit_na({{ var }})) %>% 
+    mutate(var = fct_reorder(var, n)) %>% 
+    ggplot(aes(n, var, fill = var)) + 
+    geom_col(show.legend = FALSE) +
+    scale_x_continuous(labels = scales::comma) 
+}
+
+plot_outer_inner_factor <- function(tbl, outer_var, inner_var) {
+  tbl %>% 
+    rename(outer_var = {{ outer_var }}, inner_var = {{ inner_var }}) %>% 
+    group_by(outer_var, inner_var) %>% 
+    tally() %>% 
+    ungroup() %>% 
+    mutate(outer_var = fct_reorder(outer_var, -n)) %>% 
+    mutate(inner_var = reorder_within(inner_var,
+                                      n,
+                                      outer_var)) %>% 
+    ggplot(aes(n, inner_var, fill = outer_var)) +
+    geom_col() +
+    facet_wrap(~ outer_var, scales = "free_y") +
+    scale_y_reordered() +
+    guides(fill = FALSE)
+}
