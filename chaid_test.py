b419ef08f689a01570b8b00b8b0cdd1dcb5fac9f
Marino Voćanec <51049764+MarinoVocanec@users.noreply.github.com>
2021-05-10 14:58:11 +0200

    Added imputation

--- chaid_test.py
+++ chaid_test.py
@@ -8,11 +8,18 @@ dependant_variable = gm
 super_tree = SuperCHAID(supernode_features, features_list, dependant_variable)
 super_tree.fit(df)
 
-input_row = df.loc[0]
+input_row = df.loc[11]
+input_row[make_vs_buy] = None
 print(input_row[supernode_features + features_list])
 print()
 
-segment = super_tree.predict(input_row)
+segment, segment_pairs, imputed_pairs = super_tree.predict(input_row, impute=True)
+print("Imputed pairs:", imputed_pairs)
 print("Supernode pairs:", segment.supernode_pairs)
-print("Segment pairs:", segment.segment_pairs)
+print("Segment pairs:", segment_pairs)
 print(segment)
+
+i = 0
+for _, tree in super_tree.trees.items():
+    tree.render(str(i))
+    i+=1
