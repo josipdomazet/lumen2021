280c9b05f31bb38fb88cff1607ecd89f0f7954c1
Josip Domazet <josip.domazet@live.com>
2021-05-18 17:20:16 +0200

    rest app

---
+++ rest-app/app/main.py
@@ -0,0 +1,45 @@
+import pickle
+import json
+import graphviz
+
+from flask import render_template, request, send_from_directory, jsonify
+from flask import Flask
+import pandas as pd
+
+from util import create_features
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "019a5f06d22c4ea89ce4b2177c4bc98b"
+file = None
+
+with open('./resources/chaid.model', 'rb') as handle:
+    model = pickle.load(handle)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return send_from_directory("static", "home.html")
+
+@app.route("/score", methods=["POST"])
+def score():
+    payload = request.json
+    
+    input_row, message = create_features(payload)
+    if input_row is None:
+        return message, 400
+        
+    result = model.predict(input_row)
+    if result is None:
+        return "Chosen example could not be scored.", 400
+    
+    segment, segment_pairs, imputed_pairs = result
+    output = {}
+    output["supernode_pairs"] = segment.supernode_pairs
+    output["segment_pairs"] = segment_pairs
+    output["gm_cutoffs"] = segment.gm_cutoffs
+    return jsonify(output), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
